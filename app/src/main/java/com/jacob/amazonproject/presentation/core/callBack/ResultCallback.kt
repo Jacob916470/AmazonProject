e1822029548ae4f72485644c2e09fff81787bbba
jacob <jacob.flores1676@gmail.com>
2021-07-21 22:30:27 -0500

    insert to db uploaded

--- app/src/main/java/com/jacob/amazonproject/presentation/core/callBack/ResultCallback.kt
+++ app/src/main/java/com/jacob/amazonproject/presentation/core/callBack/ResultCallback.kt
@@ -1,7 +1,10 @@
 package com.jacob.amazonproject.presentation.core.callBack
 
+/** Se crea interface llamada "ResultCallBack" la cual nos atrera un mensaje "onSuccess" y "onError".
+ * Agregamos un tipo "T" al interface*/
 interface ResultCallback<T> {
 
+    /** Creamos funciones onSuccess y onError para obtener un mensaje*/
     fun onSuccess(type: T)
     fun onError(message: String, type: T? = null)
 
